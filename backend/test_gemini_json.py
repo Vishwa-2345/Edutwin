ceb1c779e0275051432bde106064f8782c4c15fc
Vishwa-2345 <pvvishwapvvishwa@gmail.com>
2026-05-30 13:31:09 +0530

    refactor: remove obsolete test scripts and prune environment documentation from README

--- backend/test_gemini_json.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from app.services.ai_content_service import ai_generator
-
-async def test():
-    res = await ai_generator.generate_quiz_questions("Functions & Scope", num_questions=1)
-    print(res)
-
-asyncio.run(test())
